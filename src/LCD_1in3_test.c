4f30dcea5870159050b713b56b542752c8786f38
zp4rker <iam@zp4rker.com>
2023-05-03 12:35:27 +1000

    missed a semicolon lol

--- src/LCD_1in3_test.c
+++ src/LCD_1in3_test.c
@@ -36,7 +36,7 @@ void LCD_1in3_test(void)
     // /* GUI */
     printf("drawing...\r\n");
     Paint_DrawLine(0, 210, 240, 210, WHITE, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawString_EN(10, 10, "Hostname:\r\nIP:\r\nUptime:\r\nCPU:\r\nMemory:\r\nStorage:", &Font12, GRAY, WHITE)
+    Paint_DrawString_EN(10, 10, "Hostname:\r\nIP:\r\nUptime:\r\nCPU:\r\nMemory:\r\nStorage:", &Font12, GRAY, WHITE);
         
     // /*3.Refresh the picture in RAM to LCD*/
     LCD_1in3_Display(BlackImage);
